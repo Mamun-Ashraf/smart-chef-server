097995f638dc4eb82d821bce24fc8bcec0b5564f
Mamun-Ashrsf <ashrafdkia@gmail.com>
2023-02-11 21:20:27 +0600

    added services route

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// userName: smartChefDbUser
+// pass: 53NECqFkx0e61QKX
+
+
+const uri = "mongodb+srv://smartChefDbUser:53NECqFkx0e61QKX@cluster0.laf8zrf.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('smartChef').collection('services');
+        const reviewCollection = client.db('smartChef').collection('reviews');
+
+        app.get('/serviceAll', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err));
+
 
 app.get('/', (req, res) => {
     res.send('smart chef is running')
